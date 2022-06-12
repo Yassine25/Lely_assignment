599c32936b5729e6e60c50728bc8e03534ebd4d1
Yassine25 <yassine-bm@hotmail.com>
2022-06-12 13:17:35 +0200

    repository class

---
+++ Repository.py
@@ -0,0 +1,10 @@
+class Repository:
+
+    def __init__(self, id, name, url):
+        self.id = id
+        self.name = name
+        self.url = url
+        self.display_repository()
+
+    def display_repository(self):
+        print("a new repository is created with id = : ", self.id, ", name = ", self.name, ", url = ", self.url)
